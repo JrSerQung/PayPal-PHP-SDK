347ccd512471a543fafed4d852fe7dee96967bd5
JrSerQung <54682985+JrSerQung@users.noreply.github.com>
2025-12-29 13:35:06 +0000

    Update PayPalLogger.php

--- lib/PayPal/Log/PayPalLogger.php
+++ lib/PayPal/Log/PayPalLogger.php
@@ -57,13 +57,39 @@ class PayPalLogger extends AbstractLogger
         $this->initialize();
     }
 
-    public function initialize()
+   public function initialize(): void
     {
-      
+        $config = PayPalConfigManager::getInstance()->getConfigHashmap();
+        if (!empty($config)) {
+            $this->isLoggingEnabled = (isset($config['log.LogEnabled']) && $config['log.LogEnabled'] === '1');
+            if ($this->isLoggingEnabled) {
+                $this->loggerFile = !empty($config['log.FileName']) ? $config['log.FileName'] : ini_get('error_log');
+                $loggingLevel = strtoupper($config['log.LogLevel'] ?? '');
+                $this->loggingLevel = defined("\\Psr\\Log\\LogLevel::$loggingLevel") ?
+                    constant("\\Psr\\Log\\LogLevel::$loggingLevel") :
+                    LogLevel::INFO;
+            }
+        }
     }
 
-    public function log($level, $message, array $context = array())
+    /**
+     * PSR-3 compatible log method
+     */
+    public function log($level, Stringable|string $message, array $context = []): void
     {
-        
+        // If logging disabled, do nothing
+        if (!$this->isLoggingEnabled) {
+            return;
+        }
+
+        // Ensure the level is within configured levels
+        if (array_search($level, $this->loggingLevels) <= array_search($this->loggingLevel, $this->loggingLevels)) {
+            $messageString = $message instanceof Stringable ? (string)$message : $message;
+            error_log(
+                "[" . date('d-m-Y H:i:s') . "] " . $this->loggerName . " : " . strtoupper($level) . ": $messageString\n",
+                3,
+                $this->loggerFile
+            );
+        }
     }
 }
